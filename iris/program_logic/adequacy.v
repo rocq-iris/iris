d231916429f9f54144fd7ba0351065a5ef627b40
Robbert Krebbers <mail@robbertkrebbers.nl>
2026-03-26 17:35:19 +0100

    Export just `adequate` in `heap_lang.adequacy`.

--- iris/program_logic/adequacy.v
+++ iris/program_logic/adequacy.v
@@ -1,7 +1,7 @@
 From iris.algebra Require Import gmap auth agree gset coPset.
 From iris.proofmode Require Import proofmode.
 From iris.base_logic.lib Require Import wsat.
-From iris.program_logic Require Import weakestpre.
+From iris.program_logic Require Export weakestpre.
 From iris.prelude Require Import options.
 Import uPred.
 
