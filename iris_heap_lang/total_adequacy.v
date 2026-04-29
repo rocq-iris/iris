3049933941f852f43251406b47b4a893c0dc0f55
Robbert Krebbers <mail@robbertkrebbers.nl>
2026-04-29 18:09:56 +0200

    Port adequacy theorems to finally modality.

--- iris_heap_lang/total_adequacy.v
+++ iris_heap_lang/total_adequacy.v
@@ -4,8 +4,9 @@ From iris.heap_lang Require Export adequacy.
 From iris.heap_lang Require Import proofmode notation.
 From iris.prelude Require Import options.
 
-Definition heap_total Σ `{!heapGpreS Σ} s e σ φ :
-  (∀ `{!heapGS_gen HasNoLc Σ}, ⊢ inv_heap_inv -∗ WP e @ s; ⊤ [{ v, ⌜φ v⌝ }]) →
+Definition heap_total Σ `{!heapGpreS Σ} s e σ φ m :
+  (∀ `{!heapGS_gen HasLc Σ},
+    ⊢ inv_heap_inv -∗ £ m -∗ WP e @ s; ⊤ [{ v, ⌜φ v⌝ }]) →
   sn erased_step ([e], σ).
 Proof.
   intros Hwp; eapply (twp_total _ _); iIntros (?) "".
